56a0ec37670a2892f248cbba6177eddff4043d2e
Leon <phaibin@gmail.com>
2013-03-19 18:49:14 +0800

    following users

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,8 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
 
   before_save { self.email.downcase! }
   before_save :create_remember_token
@@ -27,6 +29,14 @@ class User < ActiveRecord::Base
 
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
   end
 
   private
